232fd3edb9f50591e51f17f216b78b645a8b3520
CatBoxy <catboxy@gmail.com>
2023-03-15 17:07:29 +0800

    產品細節下拉表單

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,6 +1,7 @@
 <template>
   <CNavbar></CNavbar>
   <div class="container">
+    <!-- 麵包屑 -->
     <div class="row">
       <div class="col">
         <nav aria-label="breadcrumb">
@@ -16,25 +17,53 @@
         </nav>
       </div>
     </div>
+    <!-- 產品內容 -->
     <div class="row">
-      <div class="col-sm-6">
-        <img class="img-fluid" :src="product.imageUrl" alt="">
+      <div class="col-6 col-sm-6 gx-5 d-flex justify-content-center">
+        <img class="img-fluid" :src="product.imageUrl" style="object-fit: cover;" alt="">
       </div>
-      <div class="col-sm-6">
-        <span class="badge bg-primary rounded-pill">{{ product.category }}</span>
-        <p>商品描述：{{ product.description }}</p>
-        <p>商品內容：{{ product.content }}</p>
-        <div class="h5" v-if="!product.price">{{ product.origin_price }} 元</div>
-        <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
-        <div class="h5" v-if="product.price">現在只要 {{ product.price }} 元</div>
-        <div>
-          <div class="input-group">
-            <input type="number" class="form-control" min="1" v-model="qty">
-            <button type="button" class="btn btn-primary" @click="handleAddCart">
-              <!-- <i class="fas fa-spinner fa-pulse" v-if="loading"></i> -->
-              加入購物車
-            </button>
+      <div class="col-6 col-sm-6 gx-5 ">
+        <div class="row">
+          <div class="col-12">
+            <h3>{{ product.title }}</h3>
           </div>
+          <div class="col-12 pb-1">
+            <span class="badge bg-primary rounded-pill">{{ product.category }}</span>
+          </div>
+          <div class="col-12 pb-1">
+            商品描述：{{ product.description }}
+          </div>
+          <div class="col-12 pb-1">
+            商品內容：{{ product.content }}
+          </div>
+          <div class="col-12 pb-1">
+            <div class="h5" v-if="!product.price">{{ product.origin_price }} 元</div>
+            <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
+          </div>
+          <div class="col-12 pb-1">
+            <div class="h5" v-if="product.price">現在只要 {{ product.price }} 元</div>
+          </div>
+          <div class="col-12 pb-1">
+            <div class="input-group">
+              <select class="form-select" id="selectNum" required>
+                <option selected>請選擇數量</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+              <button type="button" class="btn btn-primary" @click="handleAddCart">
+                加入購物車
+              </button>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
@@ -58,24 +87,39 @@ export default {
     return {
       id: "",
       product: {},
+      qty: 0,
     }
   },
   methods: {
     async getProduct() {
-      // console.log(this.$route.params.id);
       const { id } = this.$route.params;
       const res = await axios({
         method: 'get',
         url: `${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_BASE_PATH}/product/${id}`,
       });
-      // console.log(res);
       this.product = res.data.product;
       console.log(this.product);
     },
+    async handleAddCart() {
+      await axios({
+        method: 'post',
+        url: `${import.meta.env.VITE_BASE_URL}/v2/api/${import.meta.env.VITE_BASE_PATH}/cart`,
+        data: {
+          data: {
+            product_id: this.product.id,
+            qty: this.qty,
+          },
+        },
+      });
+      console.log("已加入購物車");
+      console.log("qty", this.qty);
+      console.log("product_id", this.product.id);
+
+    },
+
   },
   mounted() {
     this.id = this.$route.params.id;
-    // console.log(this.id);
     this.getProduct();
   },
 };
